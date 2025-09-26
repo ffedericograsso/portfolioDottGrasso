0b89834b5f1fc2bd9da7ffdcf4535ed00cd034fc
pokefedeita <fdrcgrasso@gmail.com>
2025-09-26 12:36:28 +0200

    versione 1.3

--- index.php
+++ index.php
@@ -13,14 +13,14 @@
         <div class="title">
             <h1>Dott. Domenico Leonardo Grasso - Otorino Laringoiatra</h1>
             <img src="https://cittasalute.it/wp-content/uploads/2023/01/grasso-1.jpg" alt="fotoProfilo" id="fotoProfilo">
-            <a href="contact.php" class="button">PRENOTA UNA VISITA</a>
+            <a href="#contattiTitolo" class="button">PRENOTA UNA VISITA</a>
         </div>
         <div class="content hidden">
             <h2>Biografia</h2>
             <p>Nato a Catania il 24 ottobre 1968, si e' laureato in Medicina e Chirurgia nel 1994. Ha conseguito la specializzazione in Audiologia nel 1998 presso l'Università di Catania e in Otorinolaringoiatria nel 2002 presso l'Università degli Studi di Ferrara. </p>
             <a href="pdf/cv_dottGrasso.pdf" target="_blank" class="cv">CURRICULUM</a>
             <h2 class="hidden">Tipi di visite</h2>
-            <p><div class="tipiVisita hidden"><b>Audiometria</b><br>
+            <p><div class="tipiVisita"><b>Audiometria</b><br>
               L'audiometria e' un esame non invasivo che permette di valutare la capacita' uditiva di un individuo. Viene eseguita utilizzando un apparecchio chiamato audiometro, che emette suoni a diverse frequenze e intensita'. Durante l'esame, il paziente deve segnalare ogni volta che sente il suono emesso.<br></div>
              
               <div class="tipiVisita hidden"><b>Endoscopia ORL</b><br>
@@ -34,7 +34,21 @@
 
               <div class="tipiVisita hidden"><b>Visita ORL</b><br>
               La visita otorinolaringoiatrica, o visita ORL, e' una prestazione medica che permette di valutare la salute dell'orecchio, del naso e della gola. Durante la visita, il dottore effettua una valutazione completa delle strutture dell'area testa e collo, utilizzando strumenti specifici per l'ispezione e l'esame funzionale.<br></div>
-
+              <h3 id="contattiTitolo">CHIAMA PER UN APPUNTAMENTO</h3>
+              <div class="contentContact hidden">
+                <select id="menu" onchange="mostraTelefono()">
+                    <option value="">Scegli un'opzione </option>
+                    <option value="Trieste">Studio privato TRIESTE</option>
+                    <option value="Conegliano">Centro di medicina CONEGLIANO</option>
+                    <option value="Treviso">Centro di medicina TREVISO</option>
+                    <option value="SanDona">Centro di medicina SAN DONA' DI PIAVE</option>
+                    <option value="Palmanova">Poliambulatorio San Marco PALMANOVA</option>
+                    <option value="Martignacco">Poliambulatorio Specialistico Citta' della Salute MARTIGNACCO</option>
+                    <option value="Pordenone">Centro di medicina PORDENONE</option>
+                    <option value="Monfalcone">Domus Sanitatis MONFALCONE</option>
+                    <option value="Gorizia">Nova Salus GORIZIA</option>
+                </select>
+                <div id="telefono"></div>
         </div>
    </body>
 </html>
@@ -50,4 +64,47 @@
   }, { threshold: 0.1 });
 
   hiddenElements.forEach(el => observer.observe(el));
+
+  function mostraTelefono() {
+                const menu = document.getElementById("menu").value;
+                const telefono = document.getElementById("telefono");
+
+                let numero = "";
+                switch (menu) {
+                    case "Trieste":
+                    numero = "+393519646074";
+                    break;
+                    case "Conegliano":
+                    numero = "+39043866191";
+                    break;
+                    case "Treviso":
+                    numero = "+390422698111";
+                    break;
+                    case "SanDona":
+                    numero = "+390421222221";
+                    break;
+                    case "Palmanova":
+                    numero = "+390432924814";
+                    break;
+                    case "Martignacco":
+                    numero = "+3904321833574";
+                    break;
+                    case "Pordenone":
+                    numero = "+390434554130";
+                    break;
+                    case "Monfalcone":
+                    numero = "+390481496077";
+                    break;
+                    case "Gorizia":
+                    numero = "+390481547073";
+                    break;
+                    default:
+                    numero = "";
+                }
+
+                telefono.innerHTML = numero 
+                    ? `<a href="tel:${numero}"> ${numero}</a>` 
+                    : "";
+              } 
+
 </script>
