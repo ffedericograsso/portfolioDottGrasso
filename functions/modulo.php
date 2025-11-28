5d7ed3817824b03dfa4d68ec3da5949ae1f8d1a6
pokefedeita <fdrcgrasso@gmail.com>
2025-11-28 09:29:09 +0100

    versioneFinale

--- functions/modulo.php
+++ functions/modulo.php
@@ -1,9 +1,17 @@
 <?php
+require_once(__DIR__ . "/config.php"); 
+
 session_set_cookie_params([
-    'secure' => true,      // Solo su HTTPS
-    'httponly' => true,    // Non accessibile via JS (protegge da XSS)
-    'samesite' => 'Strict' // Protegge da CSRF
+    'secure' => true,      
+    'httponly' => true,    
+    'samesite' => 'Strict' 
 ]);
 session_start();
-$db = mysqli_connect("localhost","dott","b0RRaCIn2777!?..","articolipapa");
+
+$db = mysqli_connect(DB_HOST, DB_USER, DB_PASS, DB_NAME);
+
+if (!$db) {
+    error_log("Errore connessione DB: " . mysqli_connect_error()); 
+    die("Errore di connessione al sistema.");
+}
 ?>
