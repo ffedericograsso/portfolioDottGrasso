71c07fd8bce24d3043dc2aa79ec22c10d84167ed
ffedericograsso <fdrcgrasso@gmail.com>
2026-01-30 13:58:43 +0100

    newCentroMedicina

--- location.php
+++ location.php
@@ -26,22 +26,22 @@
                 <tr>
                     <td>🏢 Studio privato TRIESTE</td>
                     <td data-label="Indirizzo"><a href="https://www.google.com/maps/place/Dottor+Grasso+Domenico+Leonardo+-+otorinolaringoiatra/@45.6529264,13.7800518,114m/data=!3m1!1e3!4m6!3m5!1s0x477b6b6a8b3308c9:0xce369379c36db723!8m2!3d45.6528946!4d13.7804349!16s%2Fg%2F11p0_cstc3" target="_blank">Via Cesare Battisti, 18, Trieste TS</a></td>
-                    <td data-label="Orari">OGNI GIORNO: 15-19</td>
+                    <td data-label="Orari">TRAMITE APPUNTAMENTO</td>
                 </tr>
                 <tr>
                     <td>🏥 Centro di medicina CONEGLIANO</td>
                     <td data-label="Indirizzo"><a href="https://www.google.com/maps/place/Centro+di+Medicina/@45.8714849,12.2769567,674m/data=!3m3!1e3!4b1!5s0x4779159f3dd5dd7f:0x26617593a25088a1!4m6!3m5!1s0x477915992e62dfa7:0x963e17eb52475d1f!8m2!3d45.8714812!4d12.279537!16s%2Fg%2F1tzztbhc" target="_blank">Viale Venezia, 87a, Conegliano TV</a></td>
-                    <td data-label="Orari">SOLO LUNEDÌ: 9-11</td>
+                    <td data-label="Orari">TRAMITE APPUNTAMENTO</td>
                 </tr>
                 <tr>
                     <td>🏥 Centro di medicina TREVISO</td>
                     <td data-label="Indirizzo"><a href="https://www.google.com/maps/place/Centro+di+Medicina/@45.6984888,12.2535032,676m/data=!3m2!1e3!4b1!4m6!3m5!1s0x477937dc8dc435d3:0xe21b278f19041ce8!8m2!3d45.6984851!4d12.2560835!16s%2Fg%2F11z9l1n9m" target="_blank">Viale della Repubblica, 10/B, Villorba TV</a></td>
-                    <td data-label="Orari">SOLO LUNEDÌ: 15-19</td>
+                    <td data-label="Orari">TRAMITE APPUNTAMENTO</td>
                 </tr>
                 <tr>
                     <td>🏥 Centro di medicina SAN DONÀ DI PIAVE</td>
                     <td data-label="Indirizzo"><a href="https://www.google.com/maps/place/Centro+di+Medicina/@45.638767,12.59542,17z/data=!3m1!5s0x477957bad7f1450d:0xa316b40014ae110!4m15!1m8!3m7!1s0x477957bb2d2ad6a5:0x243840b6b15d1c4b!2sCentro+di+Medicina!8m2!3d45.6386044!4d12.5952076!10e5!16s%2Fg%2F1tlzy_s7!3m5!1s0x477957bb2d2ad6a5:0x243840b6b15d1c4b!8m2!3d45.6386044!4d12.5952076!16s%2Fg%2F1tlzy_s7" target="_blank">Via Trasimeno, 2, San Donà di Piave VE</a></td>
-                    <td data-label="Orari">SOLO LUNEDÌ: 15-19</td>
+                    <td data-label="Orari">TRAMITE APPUNTAMENTO</td>
                 </tr>
                 <tr>
                     <td>🏛️ Poliambulatorio San Marco PALMANOVA</td>
@@ -56,18 +56,22 @@
                 <tr>
                     <td>🏥 Centro di medicina PORDENONE</td>
                     <td data-label="Indirizzo"><a href="https://www.google.com/maps/place/Centro+di+medicina/@45.9605301,12.6458004,17z/data=!3m2!4b1!5s0x477964c9ae374fc5:0x50e99fa0e2d93873!4m6!3m5!1s0x47796565e6269801:0x2e598b1b7262d598!8m2!3d45.9605264!4d12.6483807!16s%2Fg%2F11gx_h89xh" target="_blank">Via Della Ferriera, 22/D, Pordenone PN</a></td>
-                    <td data-label="Orari">SOLO LUNEDÌ: 9-12</td>
+                    <td data-label="Orari">TRAMITE APPUNTAMENTO</td>
                 </tr>
                 <tr>
                     <td>🏛️ Domus Sanitatis MONFALCONE</td>
                     <td data-label="Indirizzo"><a href="https://www.google.com/maps/place/Domus+Sanitatis+Srl/@45.7965891,13.5239691,17z/data=!3m1!4b1!4m6!3m5!1s0x477b0a1ddc2789c3:0x5d9585d98cad4f78!8m2!3d45.7965854!4d13.5265494!16s%2Fg%2F1tmqkmdt" target="_blank">Via dei Bagni, 27, Monfalcone GO</a></td>
-                    <td data-label="Orari">SOLO MERCOLEDÌ: 15-17</td>
+                    <td data-label="Orari">TRAMITE APPUNTAMENTO</td>
                 </tr>
                 <tr>
                     <td>🏛️ Nova Salus GORIZIA</td>
                     <td data-label="Indirizzo"><a href="https://www.google.com/maps/place/Nova+salus/@45.9333419,13.5983467,16z/data=!4m10!1m2!2m1!1snova+salus+gorizia!3m6!1s0x477b01e477e75e4f:0xf218c993f3abaee0!8m2!3d45.9333419!4d13.6078739!15sChJub3ZhIHNhbHVzIGdvcml6aWGSAQ5tZWRpY2FsX2NlbnRlcqoBWAoML2cvMWhqZ25rbGpsEAEqDiIKbm92YSBzYWx1cygHMh4QASIa7WcuRi5VDGP-kwIwoMaxClZV5yHnbhpES1kyFhACIhJub3ZhIHNhbHVzIGdvcml6aWHgAQA!16s%2Fg%2F11fmjmhj4f" target="_blank">Piazzale Martiri della Libertà d'Italia, 1/5, Gorizia GO</a></td>
-                    <td data-label="Orari">SOLO MERCOLEDÌ: 15-17</td>
+                    <td data-label="Orari">TRAMITE APPUNTAMENTO</td>
                 </tr>
+                <tr>
+                    <td>🏥 Centro di medicina MESTRE </td>
+                    <td data-label="Indirizzo"><a href="https://www.google.com/maps/place/Centro+di+Medicina/@45.5070927,12.2314937,1210m/data=!3m1!1e3!4m15!1m8!3m7!1s0x477eb453bd997461:0x3533e9e91b77b837!2sVia+Nilde+Iotti,+4,+30100+Venezia+VE!3b1!8m2!3d45.5075356!4d12.2297586!16s%2Fg%2F11l_1g5x49!3m5!1s0x477eb5005402083b:0xa56200e503407322!8m2!3d45.5072837!4d12.2328955!16s%2Fg%2F11x1gl5_c4?entry=ttu&g_ep=EgoyMDI2MDEyNy4wIKXMDSoASAFQAw%3D%3D" target="_blank">Via Nilde Iotti, 4, 30174 Venezia VE</a></td>
+                    <td data-label="Orari">TRAMITE APPUNTAMENTO</td>
             </tbody>
         </table>
     </section>
